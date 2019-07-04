96beb55819994cad3f9ceebcda5d8b177672507b
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 15:53:43 -0600

    Obj3Lab2

---
+++ Labs/Obj3Lab2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Obj3Lab2 {
+  public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      String name;
+
+    System.out.println("What's your name? ");
+    name = keyboard.next();
+    System.out.println("Hello " +name);
+
+
+  }
+}
