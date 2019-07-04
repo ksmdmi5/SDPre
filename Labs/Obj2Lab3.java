ba24d12501becab94ffce39952569768a443f5ed
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 14:39:37 -0600

    Obj2Lab3

---
+++ Labs/Obj2Lab3.java
@@ -0,0 +1,15 @@
+public class Obj2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Steak";
+    food2 = "Mashed Potatoes";
+    food3 = "Pumpkin Pie";
+
+    System.out.println("My Favorite Foods Are: ");
+    System.out.println("1: " +food1);
+    System.out.println("2: " +food2);
+    System.out.println("3: " +food3);
+
+  }
+}
