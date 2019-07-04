e10c6eadfc2a25c330a1f20086c2e92b37942409
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 15:50:01 -0600

    Obj3Lab1

---
+++ Labs/Obj3Lab1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Obj3Lab1 {
+  public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      String word;
+
+    System.out.println("Please type a word: ");
+    word = keyboard.next();
+    System.out.println(word);
+
+
+  }
+}
