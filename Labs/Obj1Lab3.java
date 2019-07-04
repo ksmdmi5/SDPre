e1b6ebabbf083d7da24aab2080a9c32f864cb97d
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 14:11:59 -0600

    Obj1Lab3

---
+++ Labs/Obj1Lab3.java
@@ -0,0 +1,8 @@
+public class Obj1Lab3 {
+
+  public static void main(String[] args) {
+    System.out.println("Name : Cody Fulker");
+    System.out.println("Age : 27");
+    System.out.println("State : Kansas");
+  }
+}
