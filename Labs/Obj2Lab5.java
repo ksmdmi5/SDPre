f4d90a36c86c15b322654fe55e4e39b678618bcc
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 15:27:31 -0600

    Obj2Lab5

---
+++ Labs/Obj2Lab5.java
@@ -0,0 +1,14 @@
+public class Obj2Lab5 {
+  public static void main(String[] args) {
+    double a, b, c;
+
+
+    a = 3;
+    b = 5;
+    c = ((a*a) + (b*b));
+    Math.sqrt(c);
+
+    System.out.println(c +" is the hypotenuse of a\ntriangle with sides 5.0 and 3.0");
+
+  }
+}
