61b04bd7e795cfa4c22c9f802b65dbc2d83f057c
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 14:32:47 -0600

    Obj2Lab2.java

---
+++ Labs/Obj2Lab2.java
@@ -0,0 +1,10 @@
+public class Obj2Lab2 {
+  public static void main(String[] args ){
+    String name = "Cody Fulker";
+    int age = 27;
+    String state = "Kansas";
+
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+
+  }
+}
