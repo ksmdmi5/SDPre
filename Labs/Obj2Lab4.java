28b8377d357338171789b222429d887a681e0b47
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 14:56:30 -0600

    Obj2Lab4

---
+++ Labs/Obj2Lab4.java
@@ -0,0 +1,13 @@
+public class Obj2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, avg;
+
+    num1 = 21;
+    num2 = 54;
+    num3 = 13;
+    avg = (num1 + num2 + num3) / 3;
+
+    System.out.println(avg);
+    
+  }
+}
