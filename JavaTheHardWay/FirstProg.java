6803e1a49c112f7a9ba1b410c1ccf977621a0bfa
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 13:38:34 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I, Cody Fulker, am determined to learn how to code." );
+        System.out.println( "Today's date is Monday, July 1, 2019." );
+    }
+}
