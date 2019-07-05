c20a05699e658492b7f0ecda05d66c8a446ed153
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 18:41:28 -0600

    Obj6Lab1

---
+++ Labs/Obj6Lab1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Obj6Lab1 {
+  public static void main(String [] args){
+    int c = 10;
+
+
+    System.out.print("Countdown\n");
+    while(c > 0)
+    for(c = 10; c > 0; c--)
+    System.out.println(c);
+
+  }
+}
