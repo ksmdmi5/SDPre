1d328c42c584d017d59901a62d7a0dc2be72d525
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 18:46:09 -0600

    Obj6Lab2

---
+++ Labs/Obj6Lab2.java
@@ -0,0 +1,10 @@
+public class Obj6Lab2 {
+  public static void main(String [] args){
+    int counter = 0;
+
+    while(counter <= 20)
+    for(counter = 0; counter <= 20; counter++)
+    System.out.println(counter);
+
+  }
+}
