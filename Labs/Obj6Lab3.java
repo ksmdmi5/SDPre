016bd3942a1737d7cf90bec1f47ee780d5bd697f
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-04 19:34:52 -0600

    Obj6Lab3

---
+++ Labs/Obj6Lab3.java
@@ -0,0 +1,14 @@
+public class Obj6Lab3 {
+  public static void main(String [] args){
+    int counter = 1;
+
+    {
+    while(counter <= 20)
+    for(counter = 1; counter <= 20; counter++)
+    if (counter % 2 == 0)
+      System.out.println(counter+" number is even.");
+    else
+      System.out.println(counter+" number is odd.");
+    }
+  }
+}
