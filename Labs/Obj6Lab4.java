422f7d733a2a9a44bf14ae6c4f056edf864b80e0
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-05 10:02:27 -0600

    Obj6Lab4, unclear and confusing

---
+++ Labs/Obj6Lab4.java
@@ -0,0 +1,18 @@
+public class Obj6Lab4 {
+  public static void main(String[] args) {
+
+    int count = 1;
+    int sum;
+
+    sum = count + 1;
+
+    for(count = 0; count <= 20; count++)
+
+    while (sum <= 210)
+    for (sum = 1; sum <=210; sum++)
+
+
+    System.out.println(sum);
+
+  }
+}
